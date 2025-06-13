b69013db5807158d52a0cfc6450aa189f95a7bb9
cherryrenee <sujinn1205@naver.com>
2025-06-13 14:50:03 +0900

    된건가
    최종

--- my-react-app/src/supabaseClient.ts
+++ my-react-app/src/supabaseClient.ts
@@ -1,8 +1,8 @@
-import { createClient, SupabaseClient} from '@supabase/supabase-js';
+import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
-const supabase: SupabaseClient = createClient(
-  'https://nujmthfyznertzzkntvc.supabase.co', // Supabase API URL
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51am10aGZ5em5lcnR6emtudHZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk1OTgwMzIsImV4cCI6MjA2NTE3NDAzMn0.CvQPkRRHHb3rR4Ro4kXEwCVi0DHqet_jYLQGvAt4_mw' // Supabase Anon Key
-);
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY as string;
+
+const supabase: SupabaseClient = createClient(supabaseUrl, supabaseAnonKey);
 
 export default supabase;
